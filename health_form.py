291f6516be99c7a5d226d1d8b4bd7c0fe4e11541
CruzAndrei <cruzandrei237@gmail.com>
2023-08-03 17:43:58 +0800

    Edit

--- health_form.py
+++ health_form.py
@@ -7,4 +7,6 @@ screen = Tk()
 screen.geometry("500x600")
 screen.title("Health Decleration Form")
 heading = Label(text="Personal Information", bg="grey", fg="black", width="500", height="3")
-screen.mainloop()
+heading()
+
+creen.mainloop()
