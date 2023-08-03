cebb0f6a58bf312c73018b8d3f87af65e700e23c
CruzAndrei <cruzandrei237@gmail.com>
2023-08-03 17:43:02 +0800

    heading

--- health_form.py
+++ health_form.py
@@ -6,4 +6,5 @@ from tkinter import filedialog
 screen = Tk()
 screen.geometry("500x600")
 screen.title("Health Decleration Form")
+heading = Label(text="Personal Information", bg="grey", fg="black", width="500", height="3")
 screen.mainloop()
