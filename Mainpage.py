e699cb1d06f305e3bed2a435726b9b42186e480d
CruzAndrei <cruzandrei237@gmail.com>
2023-08-03 17:59:50 +0800

    frame of the login menu

--- Mainpage.py
+++ Mainpage.py
@@ -9,7 +9,11 @@ root.geometry('500x600')
 root.title('Contact Tracing App')
 
 def login_page():
+    login_frame = tk.Frame(root)
+    login_frame.pack(pady=10)
+    login_frame.pack_propagate(False)
+
+    login_frame.configure(height=400, width=250)
 
 
-    
 root.mainloop()
