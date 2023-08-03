9950c29e1b01013049941cffbe2bfdb7f5a76be0
CruzAndrei <cruzandrei237@gmail.com>
2023-08-03 18:01:07 +0800

    Username both the input and description

--- Mainpage.py
+++ Mainpage.py
@@ -10,6 +10,16 @@ root.title('Contact Tracing App')
 
 def login_page():
     login_frame = tk.Frame(root)
+    
+    username_lb = tk.Label(login_frame, text="Enter Username: ", font=("bold", 12))
+    username_lb.place(x=60, y=20)
+
+    username = tk.Entry(login_frame, font=("bold", 15),
+                        bd=0, highlightcolor="#158aff",
+                        highlightthickness=2, highlightbackground="gray")
+    username.place(x=50, y=60, width=150, height=30)
+
+
     login_frame.pack(pady=10)
     login_frame.pack_propagate(False)
 
