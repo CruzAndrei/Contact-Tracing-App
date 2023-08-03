84e36f49d3f4eca1b71fd5a813f9796c42c1ab28
CruzAndrei <cruzandrei237@gmail.com>
2023-08-03 18:01:43 +0800

    same as the username, changed to password and height

--- Mainpage.py
+++ Mainpage.py
@@ -10,7 +10,7 @@ root.title('Contact Tracing App')
 
 def login_page():
     login_frame = tk.Frame(root)
-    
+
     username_lb = tk.Label(login_frame, text="Enter Username: ", font=("bold", 12))
     username_lb.place(x=60, y=20)
 
@@ -18,6 +18,14 @@ def login_page():
                         bd=0, highlightcolor="#158aff",
                         highlightthickness=2, highlightbackground="gray")
     username.place(x=50, y=60, width=150, height=30)
+
+    password_lb= tk.Label(login_frame, text="Enter Password: ", font=("bold",12))
+    password_lb.place(x=60, y=120)
+
+    password = tk.Entry(login_frame, font=("bold", 15),
+                        bd=0, highlightcolor="#158aff",
+                        highlightthickness=2, highlightbackground="gray",show ="*")
+    password.place(x=50, y=160, width=150, height=30)
 
 
     login_frame.pack(pady=10)
